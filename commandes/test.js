a078b89d03c83bf1f560370aeff88dc082f84329
Timnasa <Halisimd212@gmail.com>
2024-12-02 15:07:41 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "📎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Hello my name is  *𝕐𝔼𝕊𝕊𝔼ℝ 𝕄𝔻🇹🇿  * \n\n ' + "i'm a whatsapp bot multi-device created ";
-    let d = ' by *yesser Tech⚠️ *';
+    let z = 'Hello my name is  *NEHZA TECH🇹🇿  * \n\n ' + "i'm a whatsapp bot multi-device created ";
+    let d = ' by *nehza Tech⚠️ *';
     let varmess = z + d;
-    var img = 'https://i.imgur.com/rDaF5W9.jpeg';
+    var img = 'https://files.catbox.moe/wovlx8.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
