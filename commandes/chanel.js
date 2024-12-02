044635e1df3096fedcecfe1fd277ee470fb26f82
Timnasa <Halisimd212@gmail.com>
2024-12-02 14:46:50 +0300

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "channel", reaction: "🙌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *YESSER 😎_MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
-    let d = ' developpé par *Yesser Tech🏹*';
+    let z = 'Salut je m\'appelle *NEHZA_TECH* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *nehza Tech🏹*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029VaiMm7d4yltT51HS1T1G';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
@@ -15,7 +15,7 @@ console.log("mon test");
 
 });
 console.log("mon test");
-/*module.exports.commande = () => {
+"/*module.exports.commande = () => {
   var nomCom = ["test","t"]
   var reaction="☺️"
   return { nomCom, execute,reaction }
@@ -23,9 +23,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *HACKING-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *NEHZA TECH* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *Thomas*'
       let varmess=z+d
-      var img='https://telegra.ph/file/7113ddc85a26a69a7a437.jpg'
+      var img='https://files.catbox.moe/wovlx8.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
